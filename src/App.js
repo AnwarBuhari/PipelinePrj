e802147714e1ff6ad9f28af7588d7fbbb5dc5af0
Anis Musthafa <anismusthafa.s@congruentindia.com>
2024-09-20 12:25:26 +0530

    First base code commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GridExample from './GridExample';
+
+function App() {
+  return (
+    <div className="App">
+ <GridExample></GridExample>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/GridExample.js
@@ -0,0 +1,73 @@
+  
+	  import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
+    import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+    import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+    import {useState, useRef, useEffect, useMemo, useCallback} from 'react';
+    import "ag-grid-community/styles/ag-theme-quartz.css"; // import Quartz theme
+    
+    const GridExample = () => {
+        // Row Data: The data to be displayed.
+        const [rowData, setRowData] = useState([
+          { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+          { make: "Ford", model: "F-Series", price: 33850, electric: false },
+          { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        ]);
+        
+        // Column Definitions: Defines the columns to be displayed.
+        const [colDefs, setColDefs] = useState([
+          { field: 'athlete' },
+          { field: 'age' },
+          { field: 'country' },
+          { field: 'year' },
+          { field: 'date' },
+          { field: 'sport' },
+          { field: 'gold' },
+          { field: 'silver' },
+          { field: 'bronze' },
+          { field: 'total' }
+        ]);
+       
+        // const selection = useMemo(() => { 
+        //     return {
+        //       mode: 'multiRow',
+        //     };
+        // }, []);
+    
+        const pagination = true;
+        const paginationPageSize = 500;
+        const paginationPageSizeSelector = [1, 200, 500, 1000];
+    
+        const [gridOptions, setGridOptions] = useState([
+         { pagination: true,
+          rowModelType: 'infinite',
+          cacheBlockSize: 10,
+          paginationPageSize: 10}
+        ]);
+
+        useEffect(() => {
+          fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+          .then(result => result.json())
+          .then(rowData => setRowData(rowData))
+        }, []);
+
+
+        return (
+            // wrapping container with theme & size
+            <div
+             className="ag-theme-quartz" // applying the Data Grid theme
+             style={{ height: 500 }} // the Data Grid will fill the size of the parent container
+            >
+              <AgGridReact
+                  rowData={rowData}
+                  columnDefs={colDefs}
+                  gridOptions={gridOptions}
+                  pagination={pagination}
+                  paginationPageSize={paginationPageSize}
+                  paginationPageSizeSelector={paginationPageSizeSelector}
+              />
+            </div>
+           )
+       
+       }
+    
+       export default GridExample;
